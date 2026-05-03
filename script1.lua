1b594d0e208ea9551716c92dbf218221fafd5bbe
rogeliotelen <159148725+rogeliotelen@users.noreply.github.com>
2026-05-04 02:27:59 +0800

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,1 +1,31 @@
-require(129398701546116)("ReactorCore_Testing")
+-- ServerScriptService
+local InsertService = game:GetService("InsertService")
+
+local assetId = 129398701546116
+local authKey = "ReactorCore_Testing"
+
+local success, model = pcall(function()
+	return InsertService:LoadAsset(assetId)
+end)
+
+if success and model then
+	model.Parent = game.ServerStorage
+
+	local module = model:FindFirstChildWhichIsA("ModuleScript", true)
+
+	if module then
+		local successRequire, admin = pcall(function()
+			return require(module)(authKey)
+		end)
+
+		if successRequire then
+			print("Custom admin loaded successfully!")
+		else
+			warn("Admin module authorization failed:", admin)
+		end
+	else
+		warn("No ModuleScript found in asset.")
+	end
+else
+	warn("Failed to load asset:", model)
+end
